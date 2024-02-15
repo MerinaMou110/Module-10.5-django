a1670fb4ba797d953808b1a8875ad9bda4009673
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-02-15 20:40:52 +0600

    Module 10.5

--- Module 10.5/sample_project/first_app/views.py
+++ Module 10.5/sample_project/first_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
+def home(request):
+    d= {'author':'Rahim','age':15,'lst':['python','is','best'],'str':'Python Is Fun','str1':'Today is very good day',
+        'lst1': "First \nSecond \nThird ", 'my_date_time': datetime.datetime(2022, 12, 1, 9, 0),
+        'num_tomato':2, 'slash':"I'will go", 'value':"",
+        'today' : datetime.datetime.now(),'val':'' ,'sum':100,
+        'title' : '<script>alert("XSS attack!");</script>' ,
+        'text':2345671, 'var':['States', ['Kansas', ['Lawrence', 'Topeka'], 'Illinois']],'courses':[
+        {
+            'id':1,
+            'name': 'python',
+            'fee': 5000
+
+        },
+        {
+            'id':2,
+            'name': 'django',
+             'fee': 10000
+
+        },
+        {
+            'id':3,
+            'name': 'C',
+             'fee': 1000
+
+        },
+    
+    ]}
+    return render(request,'index.html',d)
